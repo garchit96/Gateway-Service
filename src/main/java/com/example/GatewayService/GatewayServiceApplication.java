9c9cc863146ffbcf133f953c5cc6a14b13469394
garchit96 <74394417+garchit96@users.noreply.github.com>
2021-09-28 19:45:17 +0530

    First commit

---
+++ src/main/java/com/example/GatewayService/GatewayServiceApplication.java
@@ -0,0 +1,27 @@
+package com.example.GatewayService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+	
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**").allowedOrigins("*");
+//			}
+//		};
+//	}
+
+}
